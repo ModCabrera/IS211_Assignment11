04e5a919ecea97ff266b0e3e562440a44253e863
Modesto Cabrera <cabreramod@gmail.com>
2015-11-14 02:18:46 -0500

    ts
    st

--- todoapp.py
+++ todoapp.py
@@ -5,6 +5,9 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
+tasks = []
+emails = []
+priorities = []
 
 @app.route('/')
 def hello_world():
@@ -14,8 +17,11 @@ def hello_world():
 @app.route('/submit', methods= ['POST'])
 def submit():
     task = request.form['task']
+    tasks.append(task)
     priority = request.form['priority']
+    priorities.append(priority)
     email = request.form['email']
+    emails.append(email)
     return redirect('/')
 
 
